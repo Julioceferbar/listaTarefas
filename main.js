ac7d233e15f7bcab08cbc9f2d9a089283395c015
Julio César <julioceferbar@gmail.com>
2021-09-06 22:49:14 -0300

    postUm

---
+++ main.js
@@ -0,0 +1,52 @@
+const inputTarefa = document.querySelector(".input-tarefa"); //seleciona
+const btnTarefa = document.querySelector(".btn-tarefa");
+const tarefas = document.querySelector(".tarefas");
+
+function criaLi() {
+  const li = document.createElement("li");
+  return li;
+}
+
+inputTarefa.addEventListener("keypress", function (e) {
+  if (e.keyCode === 13) {
+    //pegar a tecla enter sendo pressionada
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value); //se o enter for pressionado cria a tarefa
+  }
+});
+
+function limpaInput() {
+  //limpa input para incluir uma nova tarefa
+  inputTarefa.value = "";
+  inputTarefa.focus();
+}
+function criaBotaoApagar(li) {
+  li.innerText += ' ';//dar um espaço
+  const botaoApagar = document.createElement('button'); 
+  botaoApagar.innerText = 'Apagar';
+  botaoApagar.setAttribute('class', 'Apagar');
+  botaoApagar.setAttribute('title', 'Apagar esta tarefa');
+  li.appendChild(botaoApagar);
+}
+
+function criaTarefa(textoInput) {
+  const li = criaLi();
+  li.innerText = textoInput;
+  tarefas.appendChild(li);
+  limpaInput();
+  criaBotaoApagar(li);
+  salvaTarefa();
+}
+
+btnTarefa.addEventListener("click", function () {
+  if (!inputTarefa.value) return;
+  criaTarefa(inputTarefa.value);
+});
+
+document.addEventListener('click', function(e) {
+  const el = e.target;
+// apagando tarefa criada
+  if (el.classList.contains('Apagar')) {
+    el.parentElement.remove();
+  }
+})
